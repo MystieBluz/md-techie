71cfb7fafd84f9ed528bd3f133d0b9919cc19328
Misty Duhart <mystie.smith@gmail.com>
2022-03-27 04:09:04 -0400

    html and css and js files

--- javascript.js
+++ javascript.js
@@ -1,17 +1,18 @@
-const sections = document.querySelectorAll('.section');
-const controlsBtn = document.querySelectorAll('.controls');
-const sectionBtn = document.querySelectorAll('.control');
-const body = document.querySelectorAll('.main-content');
+const sectionsEl = document.querySelectorAll('.section');
+const controlsBtnEl = document.querySelectorAll('.controls');
+const controlBtnEl = document.querySelectorAll('.control');
+const bodyEl = document.querySelectorAll('.main-content');
 
-function Toggle(){
-    // Button clicked toggles the pages
-    for(let i = 0; i < controlsBtn.length; i++){
-        sectionBtn[i].addEventListener('click', () =>{
+function Toggle() {
+
+    for(let i = 0; i < controlBtnEl.length; i++) {
+        controlBtnEl[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn'
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
         })
     }
 }
-
+  
+   
 Toggle();
